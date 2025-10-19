3c1ecad03e9d12f1302122c36914b998b6b24f21
cnoah <noah.charrinbourrat@ynov.com>
2025-10-19 23:27:49 +0200

    feat(environment.py): environment class have been added (with the 3 different events)

---
+++ game/environment.py
@@ -0,0 +1,41 @@
+import random
+
+from game.player import Player
+
+
+class Environment:
+    def __init__(self, player: Player):
+        self.player = player
+
+    def random_event(self):
+        pass
+
+    def rain(self):
+        self.player.modify_thirst(-20)
+
+    def animal_encounter(self, fight: bool):
+        result = random.randint(0, 50)
+        if result > 25:
+            if fight: # The player fought the animal and won
+                self.player.modify_energy(-20)
+                self.player.modify_hunger(-20)
+            else: # The player ran away from the animal successfully
+                self.player.modify_energy(-30)
+                self.player.modify_thirst(20)
+        else:
+            if fight: # The player fought the animal and lost
+                self.player.modify_energy(-40)
+                self.player.modify_hunger(10)
+                self.player.modify_thirst(10)
+            else: # The player ran away from the animal but got hurt
+                self.player.modify_energy(-60)
+                self.player.modify_hunger(20)
+                self.player.modify_thirst(30)
+
+
+    def new_resources(self):
+        result = random.randint(0, 50)
+        if result > 25: # The player found new food
+            self.player.modify_hunger(-30)
+        else:
+            self.player.modify_hunger(30)
