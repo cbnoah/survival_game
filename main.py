e3d80cef6e27ce495e8742f4f67bcdbf1184c786
LeRaphouu <raphael.bonnet@ynov.com>
2025-10-28 15:07:59 +0100

    feat : menu au lancement du jeu

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
+from game.CLI import show_cli_menu, get_cli_user_choice
 from game.game_loop import main
 
 if __name__ == "__main__":
-    main()
+    while True:
+        show_cli_menu()
+        choice = get_cli_user_choice()
+        if choice == "1":
+            main()
+            break
+        elif choice == "2":
+            print("A faire")
+        elif choice == "3":
+            print("Quitter le jeu.")
+            break
+        else:
+            print("Choix invalide, réessayez.")
