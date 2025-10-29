139968a6e595788d6226892b932fc0a3aa751630
LeRaphouu <raphael.bonnet@ynov.com>
2025-10-29 20:05:04 +0100

    feat : The save feature has been successfully implemented.

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
-from game.CLI import show_cli_menu, get_cli_user_choice
+from game.CLI import show_cli_menu, get_cli_user_choice, show_cli_status
 from game.game_loop import main
+from game.player import Player
+from utils.save_handler import load_game, is_there_info
 
 if __name__ == "__main__":
     while True:
         show_cli_menu()
         choice = get_cli_user_choice()
         if choice == "1":
-            main()
+            main(Player("Survivant"))
             break
         elif choice == "2":
-            print("A faire")
+            if is_there_info():
+                try:
+                    player = load_game()
+                    main(Player(player.name, player.hunger, player.thirst, player.energy))
+                except Exception:
+                    print("Impossible de charger la sauvegarde.")
+            else:
+                print("Aucune sauvegarde disponible.")
         elif choice == "3":
             print("Quitter le jeu.")
             break
